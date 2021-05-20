919eeadd7cc4c0030674bc91843e8ece995dcc70
Renato Marinho <renato.marinhofr@gmail.com>
2021-05-20 16:50:03 -0300

    Listando os nomes dos campeoes

--- semana7/leagueinfo/src/App.js
+++ semana7/leagueinfo/src/App.js
@@ -5,22 +5,48 @@ import axios from 'axios'
 export default class App extends React.Component {
 
   state = {
-    champions: {}
+    champions: [],
+    image: ''
   }
 
   componentDidMount() {
     this.getChampions()
+    this.getChampionImages()
   }
 
   getChampions = async () => {
-    const response = await axios.get('http://ddragon.leagueoflegends.com/cdn/11.10.1/data/pt_BR/champion.json')
-    console.log('Champions: ', response.data.data)
+    try {
+      const response = await axios.get('http://ddragon.leagueoflegends.com/cdn/11.10.1/data/pt_BR/champion.json')
+      this.setState({ champions: Object.values(response.data.data) })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  getChampionImages = () => {
+    const getImage = this.state.champions.map((champion) => {
+      return this.setState ({ image: champion.image })
+    })    
   }
 
   render() {
+    //console.log(this.state.champions)
+    const championList = this.state.champions.map((champion) => {
+      return (
+        <option key={champion.name} value={champion.image}>
+          {champion.name}
+        </option>
+      )
+    })
+    //console.log(championList)
     return (
       <div className="App">
         <h1>Champions League of Legends</h1>
+        <select>
+          <option>Selecione um Campeão</option>
+          {championList}
+        </select>
+        <img src={this.state.image}/>
       </div>
     );
   }
