6980c839970e4c7d212e1d3ae70710ed8580a3be
Renato Marinho <renato.marinhofr@gmail.com>
2021-08-12 12:00:24 -0300

    Cookenu

--- semana19/Cookenu/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { app } from "./app";
-import { getAllCharacters } from "./endpoints/getAllCharacters";
-import { login } from "./endpoints/login";
-import { signup } from "./endpoints/signup";
-
-app.get("/user", getAllCharacters);
-app.post("/user", signup);
-app.post("/user/login", login);
